c920896d14867961a31a2d197ac63045a8dad560
Conrad Trost <ct.conradtrost@gmail.com>
2023-03-08 18:26:52 -0600

    feat(ci): build aws infra

---
+++ README.md
@@ -0,0 +1,7 @@
+# CDKTF AWS
+
+## Usage
+
+Install [Terraform](https://developer.hashicorp.com/terraform/downloads) and [CDKTF](https://developer.hashicorp.com/terraform/tutorials/cdktf/cdktf-install)
+
+Add credentials to ~/.aws/credentials, [see here](https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html)
--- main.ts
+++ main.ts
@@ -1,14 +1,28 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { App, TerraformStack, TerraformOutput } from "cdktf";
+import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
+import { Instance } from "@cdktf/provider-aws/lib/instance";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    new AwsProvider(this, "AWS", {
+      region: "us-east-1",
+    });
+
+    const ec2Instance = new Instance(this, "compute", {
+      ami: "ami-006dcf34c09e50022",
+      instanceType: "t2.micro",
+    });
+
+    new TerraformOutput(this, "public_ip", {
+      value: ec2Instance.publicIp,
+    });
   }
 }
 
 const app = new App();
-new MyStack(app, "cdktf-aws");
+new MyStack(app, "aws_instance");
+
 app.synth();
